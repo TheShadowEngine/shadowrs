fb9562450bf2bd960ae83a25dca587ae5019baab
krishpranav <krisna.pranav@gmail.com>
2023-06-10 16:24:35 +0530

    crates: shadow_license[cli]

--- crates/shadow_license/src/main.rs
+++ crates/shadow_license/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("hello world")
+use anyhow::Result;
+use clap::Parser;
+use duct::cmd;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+pub enum Args {
+    #[clap(name = "generate")]
+    Generate(GenerateArgs),
+}
+
+#[derive(Parser)]
+pub struct GenerateArgs {
+    #[clap(short, long, help = "the path to write the license")]
+    pub output: PathBuf,
+}
+
+fn generate(generate_args: GenerateArgs) -> Result<()> {
+    let private_key = cmd!("gpg", "--decrypt", "secrets/keys/license.private.rsa")
+        .run()?
+        .stdout;
+    let private_key = String::from_utf8(private_key)?;
+    let license = shadow_license::generate(&private_key)?;
+    std::fs::write(generate_args.output, license)?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    match args {
+        Args::Generate(generate_args) => generate(generate_args)?,
+    };
+    Ok(())
 }
