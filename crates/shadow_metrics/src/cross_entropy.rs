2b51b346747febbbc87f9eef2dfdc60afa34c0f7
krishpranav <krisna.pranav@gmail.com>
2023-06-11 15:37:39 +0530

    crates: shadow_metrics[cross_entropy]

--- crates/shadow_metrics/src/cross_entropy.rs
+++ crates/shadow_metrics/src/cross_entropy.rs
@@ -20,7 +20,22 @@ pub struct CrossEntropyInput<'a> {
 pub struct CrossEntropyOutput(pub Option<f32>);
 
 impl CrossEntropy {
+    pub fn update(&mut self, value: CrossEntropyInput) {
+        let label = value.label.unwrap().get() - 1;
+        let mut total = 0.0;
+        for (index, &probability) in value.probabilities.indexed_iter() {
+            if index == label {
+                total += -clamp(probability, std::f32::EPSILON, 1.0 - std::f32::EPSILON).ln();
+            }
+        }
+        self.0.update(total)
+    }
+
     pub fn merge(&mut self, other: CrossEntropy) {
-        self.0.merge(other.0);
+        self.0.merge(other.0)
+    }
+
+    pub fn finalize(self) -> CrossEntropyOutput {
+        CrossEntropyOutput(self.0.finalize())
     }
 }
