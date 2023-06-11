f4beed31a3956d0dcb6be2167965566805526a18
krishpranav <krisna.pranav@gmail.com>
2023-06-11 15:34:32 +0530

    crates: shadow_metrics: regression

--- crates/shadow_metrics/src/regression.rs
+++ crates/shadow_metrics/src/regression.rs
@@ -30,3 +30,39 @@ impl Default for RegressionMetrics {
         }
     }
 }
+
+impl RegressionMetrics {
+    pub fn new() -> RegressionMetrics {
+        RegressionMetrics::default()
+    }
+
+    pub fn update(&mut self, input: RegressionMetricsInput) {
+        for (prediction, label) in zip!(input.predictions.iter(), input.labels.iter()) {
+            self.mean_variance.update(*label);
+            let absolute_error = prediction - label;
+            let squared_error = absolute_error * absolute_error;
+            self.absolute_error += absolute_error as f64;
+            self.squared_error += squared_error as f64;
+        }
+    }
+
+    pub fn merge(&mut self, other: RegressionMetrics) {
+        self.mean_variance.merge(other.mean_variance);
+        self.absolute_error += other.absolute_error;
+        self.squared_error += other.squared_error;
+    }
+
+    pub fn finalize(self) -> RegressionMetricsOutput {
+        let MeanVarianceOutput { variance, n, .. } = self.mean_variance.finalize();
+        let mae = self.absolute_error / n.to_f64().unwrap();
+        let mse = self.squared_error / n.to_f64().unwrap();
+        let rmse = mse.sqrt();
+        let r2 = 1.0 - self.squared_error / (variance as f64 * n.to_f64().unwrap());
+        RegressionMetricsOutput {
+            mae: mae.to_f32().unwrap(),
+            mse: mse.to_f32().unwrap(),
+            r2: r2.to_f32().unwrap(),
+            rmse: rmse.to_f32().unwrap(),
+        }
+    }
+}
