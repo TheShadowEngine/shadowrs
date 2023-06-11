23ebc2f286a4bb39c69e91eea7594a1ab7e606d9
krishpranav <krisna.pranav@gmail.com>
2023-06-11 12:51:32 +0530

    crates: shadow_metrics[accuracy]

--- crates/shadow_metrics/src/accuracy.rs
+++ crates/shadow_metrics/src/accuracy.rs
@@ -2,3 +2,23 @@ use super::mean::Mean;
 
 #[derive(Default)]
 pub struct Accuracy(Mean);
+
+impl Accuracy {
+    pub fn new() -> Accuracy {
+        Accuracy::default()
+    }
+}
+
+impl Accuracy {
+    pub fn update(&mut self, value: (usize, usize)) {
+        self.0.update(if value.0 == value.1 { 1.0 } else { 0.0 })
+    }
+
+    pub fn merge(&mut self, other: Accuracy) {
+        self.0.merge(other.0)
+    }
+
+    pub fn finalize(self) -> Option<f32> {
+        self.0.finalize()
+    }
+}
