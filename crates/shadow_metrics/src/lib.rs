056c0077964f263a8911e4026cab3c7b6da2a031
krishpranav <krisna.pranav@gmail.com>
2023-06-11 15:45:25 +0530

    crates: shadow_metrics[lib]

--- crates/shadow_metrics/src/lib.rs
+++ crates/shadow_metrics/src/lib.rs
@@ -1,5 +1,27 @@
+pub use self::{
+    accuracy::Accuracy,
+    auc_roc::*,
+    binary_classification::{
+        BinaryClassificationMetrics, BinaryClassificationMetricsInput,
+        BinaryClassificationMetricsOutput, BinaryClassificationMetricsOutputForThreshold,
+    },
+    binary_cross_entropy::{BinaryCrossEntropy, BinaryCrossEntropyInput},
+    cross_entropy::{CrossEntropy, CrossEntropyInput, CrossEntropyOutput},
+    mean::Mean,
+    mean_errors::MeanSquaredError,
+    mean_variance::{m2_to_variance, merge_mean_m2, MeanVariance},
+    mode::Mode,
+    multiclass_classi::{
+        ClassMetrics, MulticlassClassificationMetrics, MulticlassClassificationMetricsInput,
+        MulticlassClassificationMetricsOutput,
+    },
+    regression::{RegressionMetrics, RegressionMetricsInput, RegressionMetricsOutput},
+};
+
 mod accuracy;
+mod auc_roc;
 mod binary_classification;
+mod binary_cross_entropy;
 mod cross_entropy;
 mod mean;
 mod mean_errors;
