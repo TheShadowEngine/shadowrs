019491ebd5fa2d5236452de576b22705790ea49c
krishpranav <krisna.pranav@gmail.com>
2023-06-12 19:11:22 +0530

    crates: shadow_linear[shap]

---
+++ crates/shadow_linear/src/shap.rs
@@ -0,0 +1,29 @@
+use ndarray::prelude::*;
+use shadow_zip::zip;
+
+pub struct ComputeShapValuesForExampleOutput {
+    pub baseline_value: f32,
+    pub output_value: f32,
+    pub feature_contribution_values: Vec<f32>,
+}
+
+pub fn compute_shap_values_for_example(
+    features: &[f32],
+    bias: f32,
+    weights: ArrayView1<f32>,
+    means: &[f32],
+) -> ComputeShapValuesForExampleOutput {
+    let baseline_value = bias
+        + zip!(weights, means)
+            .map(|(weight, mean)| weight * mean)
+            .sum::<f32>();
+    let feature_contributions: Vec<f32> = zip!(weights, features, means)
+        .map(|(weight, feature, mean)| weight * (feature - mean))
+        .collect();
+    let output_value = baseline_value + feature_contributions.iter().sum::<f32>();
+    ComputeShapValuesForExampleOutput {
+        baseline_value,
+        output_value,
+        feature_contribution_values: feature_contributions,
+    }
+}
